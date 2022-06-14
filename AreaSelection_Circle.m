62842953be7e56851291b0044feb441cd0583b39
karemr2024 <75687693+karemr2024@users.noreply.github.com>
2022-06-14 12:10:08 -0400

    AreaSelection_Circle add

---
+++ AreaSelection_Circle.m
@@ -0,0 +1,15 @@
+clc; clearvars;
+% function [I2,dataX,dataYY] = AreaSelection(b)
+ GH = figure; imshow(R_Tiff_0nm)%%%Shows the input image%%%%
+  waitforbuttonpress  %%%%%%Press left click on the image%%%%
+  ss = size(b);
+  point1 = get(gcf,'CurrentPoint') ;
+  rect = [point1(1,1) point1(1,2) 64 64]; %Coordinates of rectangle%
+  [r2] = dragrect(rect);                    %%%Drag the rectangle while keeping leftclick pressed and leave the click when region to be selected is decided%%
+  [dataX, dataY] = pix2data(r2(1,1),r2(1,2));
+  ggr = ss(1,1) - dataY ;
+  dataY = ggr+0.5;
+  dataX=round(dataX+0.5);                      %Top left hand side X coordinate of the image%
+  dataYY =round(dataY-r2(1,4));                %Top left hand side Y coordinate of the image%  
+  I2 = imcrop(b,[dataX dataYY 63 63]);%Final Cropped image%
+  close(GH)
