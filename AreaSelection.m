86f7bf652bd9e3600816fa487a67afd2a4ee167f
erenergene <erenergene@dhcp-wifi-8021x-155-41-99-160.bu.edu>
2022-06-16 16:09:32 -0400

    changed function header

--- AreaSelection.m
+++ AreaSelection.m
@@ -1,7 +1,7 @@
-% function [I2,dataX,dataYY] = AreaSelection(b)
- GH = figure; imshow(R_Tiff_0nm_Crop)%%%Shows the input image%%%%
+function [I2,dataX,dataYY] = AreaSelection(b)
+ GH = figure; imshow(b)%%%Shows the input image%%%%
   waitforbuttonpress  %%%%%%Press left click on the image%%%%
-  ss = size(R_Tiff_0nm_Crop);
+  ss = size(b);
   point1 = get(gcf,'CurrentPoint') 
   rect = [point1(1,1) point1(1,2) 64 64]; %Coordinates of rectangle%
   [r2] = dragrect(rect);                    %%%Drag the rectangle while keeping leftclick pressed and leave the click when region to be selected is decided%%
@@ -10,5 +10,5 @@
   dataY = ggr+0.5;
   dataX=round(dataX+0.5);                      %Top left hand side X coordinate of the image%
   dataYY =round(dataY-r2(1,4));                %Top left hand side Y coordinate of the image%  
-  I2 = imcrop(R_Tiff_0nm_Crop,[dataX dataYY 63 63]);%Final Cropped image%
+  I2 = imcrop(b,[dataX dataYY 63 63]);%Final Cropped image%
   close(GH)
